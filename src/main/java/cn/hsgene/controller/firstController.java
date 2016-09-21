5bd2daeb03f180efd7f47ea26fd8f42aa2103eec
韩俊辰 <812878994@qq.com>
2016-09-21 18:02:03 +0800

    1

--- src/main/java/cn/hsgene/controller/firstController.java
+++ src/main/java/cn/hsgene/controller/firstController.java
@@ -1,4 +1,4 @@
-package cn.hsgene;
+package cn.hsgene.controller;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
@@ -9,14 +9,13 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Created by hjc on 2016/9/21.
  */
-public class myFirstController extends AbstractController{// BeanNameUrlHandlerMapping方式才需要继承AbstractController
+public class firstController extends AbstractController{// BeanNameUrlHandlerMapping方式才需要继承AbstractController
 
     // handleRequestInternal方法相当于struts中的excute方法，即不指定访问方法默认响应请求的方法
     @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse) throws Exception{
-
         System.out.println("test first springMVC!");
-        return new ModelAndView("firstSpringMVC");// 此处的参数是逻辑视图名，会交给InternalResourceViewResolver进行解析
+        return new ModelAndView("springmvc01");// 此处的参数是逻辑视图名，会交给InternalResourceViewResolver进行解析
     }
 
 }
