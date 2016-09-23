fbfdd15d122cc5acf5b104306f1b12680d8f7c33
韩俊辰 <812878994@qq.com>
2016-09-23 18:03:10 +0800

    build springMVC

---
+++ src/main/java/com/hsgene/controller/MyController.java
@@ -0,0 +1,21 @@
+package com.hsgene.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by hjc on 2016/9/21.
+ */
+public class MyController extends AbstractController{// BeanNameUrlHandlerMapping方式才需要继承AbstractController
+
+    // handleRequestInternal方法相当于struts中的excute方法，即不指定访问方法默认响应请求的方法
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse) throws Exception{
+        System.out.println("test springMVC!");
+        return new ModelAndView("springmvc02");// 此处的参数是逻辑视图名，会交给InternalResourceViewResolver进行解析
+    }
+
+}
