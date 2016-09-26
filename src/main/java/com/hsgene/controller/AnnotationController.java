5e202309f50256e9cddff1ba71e564efef197b79
韩俊辰 <812878994@qq.com>
2016-09-26 17:52:36 +0800

    springmvc

--- src/main/java/com/hsgene/controller/AnnotationController.java
+++ src/main/java/com/hsgene/controller/AnnotationController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by hjc on 2016/9/25.
  */
 @Controller
-@RequestMapping("/annotationController") // 加上/表示根目录
+//@RequestMapping(value = "/annotationController") // 加上/表示根目录
 public class AnnotationController {// 注解方式不需要继承任何接口和抽象类
 
-    @RequestMapping("index") // 加上/表示根目录，是绝对路径，不加/表示相对路径，相对Controller的路径
+    @RequestMapping(value = "index") // 加上/表示根目录，是绝对路径，不加/表示相对路径，相对Controller的路径
     public String index(){
         System.out.println("测试注解方式配置控制器！");
         return "springmvc02";
