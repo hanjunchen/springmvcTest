f577a553ddfe30c8d5eecac45fd257927d261d80
韩俊辰 <812878994@qq.com>
2016-09-27 14:25:11 +0800

    springmvc

--- src/main/java/com/hsgene/controller/AnnotationController.java
+++ src/main/java/com/hsgene/controller/AnnotationController.java
@@ -4,13 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 /**
  * Created by hjc on 2016/9/25.
  */
 @Controller
 //@RequestMapping(value = "/annotationController") // 加上/表示根目录
-public class AnnotationController {// 注解方式不需要继承任何接口和类
+public class AnnotationController { // 注解方式不需要继承任何接口和类
+
+    private String userName;    // struts2采用这种方式传参，可以通过注解@Scope(“prototype”)设置action为多例，而springMVC的Controller采用的是单例，所以在性能上springMVC比struts快的多
+    // Controller支持多线程，springMVC控制器中应尽量避免使用成员变量，否则变量共享后将数据不安全，方法上如果不加synchronized方法是不同步的，即多个用户访问互不影响
 
     @RequestMapping(value = "/index")   // 加上/表示根目录，是绝对路径，不加/表示相对路径，相对Controller的路径
     public String index(@RequestParam(value = "id", required = false) String id) {  // @RequestParam注解指定传入的参数，默认required参数为true，所以默认必须传参
@@ -19,11 +25,30 @@ public class AnnotationController {// 注解方式不需要继承任何接口和
     }
 
     @RequestMapping(value = "/test1")
-    public String test1(String name,Model model) {    // @RequestParam注解默认是可以省略的，并且默认的required值为false，即不必需传参
-        System.out.println("hello --- " + name);
-        name = "哈哈";
-        model.addAttribute("name",name);    // spring自身的Model数据模型，Map结构传递数据，会随着逻辑视图名被视图解析器解析，可以使用EL表达式输出，由此该跳转是页面转发
-        model.asMap().values().forEach(x-> System.out.println(x));
+    public String test1(String name, Model model) {    // @RequestParam注解默认是可以省略的，并且默认的required值为false，即不必需传参
+        System.out.println("test1 --- " + name);
+        model.addAttribute("name", name);    // spring自身的Model数据模型，Map结构传递数据，会随着逻辑视图名被视图解析器解析，可以使用EL表达式输出，由此该跳转是页面转发
+        model.addAttribute(name);
+        model.asMap().values().forEach(x -> System.out.println(x));
+        return "springmvc";
+    }
+
+    @RequestMapping(value = "/test2")
+    public ModelAndView test1(String name) {    // 这种方式就相当于上面的Model方式，将数据模型和逻辑视图名显示封装在ModelAndView中并返回，Model方式是隐式创建Model实例后在逻辑视图名中携带数据模型并返回
+        System.out.println("test2 --- " + name);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("springmvc");
+        modelAndView.addObject("name", name);
+        modelAndView.addObject(name);   // 这种不指定key值，springmvc默认将key值设为数据类型的小写，即相当于modelAndView.addObject("string",name);
+        // modelAndView.addObject(new User()); 常用于将对象直接加入模型中，默认的key值是类小写-->modelAndView.addObject("user",new User());
+        // key、value设值规则和Model一样，Model设值也可以不指定key，具有同样的默认值
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/test3")
+    public String test1(String name, Map<String, Object> map) { // 将Model替换成Map同样可行，因为Model本身就是一个Map，一般不用这种方式
+        System.out.println("test3 --- " + name);
+        map.put("name", name);
         return "springmvc";
     }
 }
--- src/main/webapp/WEB-INF/jsp/springmvc.jsp
+++ src/main/webapp/WEB-INF/jsp/springmvc.jsp
@@ -5,8 +5,9 @@
   Time: 16:54
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page isELIgnored="false" %><!-- jsp版本默认忽略EL表达式，默认值为true，需要设为false才能使用EL表达式 -->
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page isELIgnored="false" %><!-- jsp版本默认忽略EL表达式，默认值为true，需要设为false才能使用EL表达式 -->
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%><!-- 导入jstl jar包 -->
 <html>
 <head>
   <title>111</title>
@@ -14,5 +15,7 @@
 <body>
 <h1>注解扫描controller方式！</h1>
 <p>${name}</p>
+<p>${string}</p>
+<c:out value="${name}"></c:out>
 </body>
 </html>
