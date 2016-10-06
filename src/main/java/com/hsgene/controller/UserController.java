25f574829ed7351c06344feb951571a70b32a7de
hanjunchen <812878994@qq.com>
2016-10-06 10:44:08 +0800

    springmvc

--- src/main/java/com/hsgene/controller/UserController.java
+++ src/main/java/com/hsgene/controller/UserController.java
@@ -74,14 +74,14 @@ public class UserController {
         return "userList";
     }
 
-    @RequestMapping(value = "addPage",method = RequestMethod.GET)
+    @RequestMapping(value = "add",method = RequestMethod.GET)
     // @ModelAttribute作用就相当于model.addAttribute(new User())，请求进来没有参数，但是仍会实例化一个空的User对象，通过这个注解将其添加到Model中，一般用在不需要改变对象或者需要对象初始状态的情况
-    public String addPage(@ModelAttribute("user")User user){ // 这个方法是为了给添加页面一个空的User对象模型，从而方便表单提交时的自动封装
+    public String add(@ModelAttribute("user")User user){ // 这个方法是为了给添加页面一个空的User对象模型，从而方便表单提交时的自动封装
         return "adduser";
     }
 
-    @RequestMapping(value = "add",method = RequestMethod.POST)  // 表单提交使用post方式
-    public String add(@Validated User user,BindingResult bindingResult){//  注意@Validated和BindingResult对象必须紧跟在一起
+    @RequestMapping(value = "addSave",method = RequestMethod.POST)  // 表单提交使用post方式
+    public String addSave(@Validated User user,BindingResult bindingResult){//  注意@Validated和BindingResult对象必须紧跟在一起
         if(bindingResult.hasErrors()){
             return "adduser";
         }
--- src/main/webapp/WEB-INF/jsp/adduser.jsp
+++ src/main/webapp/WEB-INF/jsp/adduser.jsp
@@ -13,11 +13,11 @@
     <title>add</title>
 </head>
 <body>
-<%-- modelAttribute是从后台接收到一个空的对象模型 --%>
-<form:form action="/user/add" modelAttribute="user" method="post">
+<%-- modelAttribute是从后台接收到一个空的对象模型，不指定action的话是 --%>
+<form:form modelAttribute="user" method="post">
     <%-- path相当于普通input中的name，都是modelAttribute指定模型的属性名称，默认type是text类型 --%>
     id:<form:input path="id"/><br>
-    name:<form:input path="name"/><form:errors path="name"/><br><%-- 这里报红是正确的，因为在idea中的原因 -->
+    name:<form:input path="name"/><form:errors path="name"/><br><%-- path名与对应表单name值相同，注意：JSR303的错误验证信息显示只能通过springMVC的标签来显示，而其他的表单元素则可以用普通标签来代替 -->
     <%--password:<fm:password path="password"/><br> 这是密码类型，其他类型都有对应名称的标签--%>
     age:<form:input path="age"/><br>
     <input type="submit" value="保存"/>
